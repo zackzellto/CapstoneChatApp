1bd571f9fc93596062e0eca2d828746a40e84a95
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-03-08 13:01:07 -0700

    updating for testing with swagger

--- CapstoneChatApp/Models/ChatUsers.cs
+++ CapstoneChatApp/Models/ChatUsers.cs
@@ -11,7 +11,7 @@ namespace ReactChatApp.Models
         public virtual string first_name { get; set; } = string.Empty;
         public virtual string last_name { get; set; } = string.Empty;
         public virtual string email { get; set; } = string.Empty;
-        public virtual byte[] password { get; set; }
+        public virtual string password { get; set; }
 
     }
 }
